53659d250e01836473a93a26ec01fd95713ad46a
anisaasaluka <anisaasaluka@gmail.com>
2026-04-10 00:30:24 +0700

    Add files via upload

--- pertemuan2/Mahasiswa.java
+++ pertemuan2/Mahasiswa.java
@@ -1,1 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan2;
 
+/**
+ *
+ * @author Anisa
+ */
+public class Mahasiswa {
+   String nim;
+    String nama;
+    String jurusan;
+
+    public Mahasiswa(String nim, String nama, String jurusan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.jurusan = jurusan;
+    }
+
+    public void tampilData() {
+        System.out.printf("%-15s %-20s %-20s\n", nim, nama, jurusan);
+    }
+}
